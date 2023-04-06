f5ac5d1cc27564fd11afcaf1bb6263cd55bc1399
Omar Zeghouani <omarzeghouanii@gmail.com>
2023-04-06 22:45:42 +0100

    Add npm package

---
+++ src/filter.ts
@@ -0,0 +1,151 @@
+import {
+  createReadStream,
+  createWriteStream,
+  existsSync,
+  mkdirSync,
+  readFileSync,
+  unlinkSync,
+  writeFileSync,
+} from "fs";
+import * as pandoc from "pandoc-filter";
+import { join } from "path";
+import { fileSync } from "tmp";
+var exec = require("child_process").execSync;
+
+var counter = 0;
+const folder = process.cwd();
+const errorLog = createWriteStream(join(folder, "d2-filter.err"));
+
+enum D2Theme {
+  NeutralDefault = 0,
+  NeutralGrey = 1,
+  FlagshipTerrastruct = 3,
+  CoolClassics = 4,
+  MixedBerryBlue = 5,
+  GrapeSoda = 6,
+  Aubergine = 7,
+  ColorblindClear = 8,
+  VanillaNitroCola = 100,
+  OrangeCreamsicle = 101,
+  ShirelyTemple = 102,
+  EarthTones = 103,
+  EvergladeGreen = 104,
+  ButteredToast = 105,
+  DarkMauve = 200,
+  Terminal = 300,
+  TerminalGrayscale = 301,
+  Origami = 302,
+}
+
+enum D2Layout {
+  Dagre = "dagre",
+  Elk = "elk",
+}
+
+enum D2Format {
+  Svg = "svg",
+  Png = "png",
+  Pdf = "pdf",
+}
+
+type FilterOptions = {
+  theme: D2Theme;
+  layout: D2Layout;
+  format: D2Format;
+  sketch: boolean;
+  folder?: string;
+  filename?: string;
+  caption?: string;
+};
+
+const action: pandoc.SingleFilterActionAsync = async function (elt, _format) {
+  if (elt.t != "CodeBlock") return undefined;
+  const attrs = elt.c[0];
+  const content = elt.c[1];
+  const id = attrs[0];
+  const classes = attrs[1];
+  const options: FilterOptions = {
+    theme: D2Theme.NeutralDefault,
+    layout: D2Layout.Dagre,
+    format: D2Format.Svg,
+    sketch: false,
+  };
+
+  if (classes.indexOf("d2") < 0) return undefined;
+
+  attrs[2].map((item) => {
+    switch (item[0]) {
+      case "theme":
+        if (+item[1] in D2Theme) options.theme = +item[1];
+        break;
+      case "sketch":
+        options.sketch = item[1] === "true";
+        break;
+      case "layout":
+        if (item[1] in D2Layout) options.layout = item[1] as D2Layout;
+        break;
+      case "format":
+        if (item[1] in D2Format) options.format = item[1] as D2Format;
+        break;
+      case "folder":
+      case "filename":
+      case "caption":
+        options[item[0]] = item[1];
+        break;
+    }
+  });
+
+  counter++;
+
+  const tmpFile = fileSync();
+  writeFileSync(tmpFile.name, content);
+  const outDir = options.folder ?? "";
+
+  if (options.filename === "") {
+    options.filename = `diagram-${counter}`;
+  }
+
+  const savePath = tmpFile.name + "." + options.format;
+  var newPath = join(outDir, `${options.filename}.${options.format}`);
+  const fullCmd = `d2 -t=${options.theme} -l=${options.layout} -s=${options.sketch} ${tmpFile.name} ${savePath}`;
+  exec(fullCmd);
+
+  if (options.folder == "") {
+    if (options.format === "svg") {
+      const data = readFileSync(savePath, "utf8");
+      newPath =
+        "data:image/svg+xml;base64," + Buffer.from(data).toString("base64");
+    } else if (options.format === "pdf") {
+      newPath = savePath;
+    } else {
+      const data = readFileSync(savePath);
+      newPath = "data:image/png;base64," + Buffer.from(data).toString("base64");
+    }
+  } else {
+    const imageFolder = join(folder, outDir);
+    if (!existsSync(imageFolder)) {
+      mkdirSync(imageFolder);
+    }
+    const readStream = createReadStream(savePath);
+    const writeStream = createWriteStream(newPath);
+    readStream.on("close", () => {
+      unlinkSync(savePath);
+    });
+    readStream.pipe(writeStream);
+  }
+
+  const fig = options.caption ? "fig:" : "";
+  return pandoc.Para([
+    pandoc.Image(
+      [id, [], []],
+      [pandoc.Str(options.caption ?? "")],
+      [newPath, fig]
+    ),
+  ]);
+};
+
+export = function () {
+  // @ts-ignore
+  process.stderr.write = errorLog.write.bind(errorLog);
+  pandoc.stdio(action);
+};
